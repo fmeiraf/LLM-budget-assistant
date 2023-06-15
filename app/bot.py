5b643fb8c763f30be5961754fd5ec54b487e76dd
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-06-14 21:50:10 -0600

    revamped on_message method and added the first view for the buttons

--- app/bot.py
+++ app/bot.py
@@ -9,11 +9,51 @@ class BotClient(discord.Client):
     async def on_ready(self):
         print(f"{self.user} is ready and online!")
 
+    def is_bot_own_message(self, message):
+        # we do not want the bot to reply to itself
+        return message.author.id == self.user.id
+
+    async def starter_message(self, message):
+        starter_message = await message.channel.send(
+            content=f"Hi {message.author.mention}  😎 \n \n Let's start working! What would you like to do now? Chose between the options below: \n ",
+            reference=message,
+            view=StarterView(),
+        )
+        self.starter_message_id = starter_message.id
+        return starter_message
+
     async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if self.is_bot_own_message(message):
+            return
+
         if message.content == "hi":
-            await message.channel.send(
-                f"hello {message.author.mention}", reference=message
-            )
+            await self.starter_message(message)
+
+        if (
+            message.reference
+            and message.reference.message_id == self.starter_message_id
+        ):
+            await message.channel.send("tuche")
+
+
+class StarterView(
+    discord.ui.View
+):  # Create a class called MyView that subclasses discord.ui.View
+    @discord.ui.button(
+        label="Add transactions",
+        row=0,
+        style=discord.ButtonStyle.primary,
+        emoji="💸",
+    )
+    async def first_button_callback(self, button, interaction):
+        await interaction.response.send_message("You pressed me!")
+
+    @discord.ui.button(
+        label="Update transactions", row=0, style=discord.ButtonStyle.secondary, emoji="🔄"
+    )
+    async def second_button_callback(self, button, interaction):
+        await interaction.response.send_message("You pressed me!")
 
 
 # bot = discord.Bot(intents=intents)
@@ -21,19 +61,3 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = BotClient(intents=intents)
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))  # run the bot with the token
-
-
-# @bot.event
-# async def on_ready():
-#     print(f"{bot.user} is ready and online!")
-
-
-# @bot.slash_command(name="hello", description="Say hello to the bot")
-# async def hello(ctx):
-#     await ctx.respond("Hey!")
-
-
-# @bot.event
-# async def on_message(message):
-#     if message.content == "hi":
-#         await message.channel.send("hello", reference=message)
