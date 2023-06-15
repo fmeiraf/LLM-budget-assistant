04d90381bb921033e89b3e89307002e0acb590e5
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-06-14 19:57:36 -0600

    converted the bot code to use subclass

--- app/bot.py
+++ app/bot.py
@@ -3,34 +3,37 @@ import os  # default module
 from dotenv import load_dotenv
 
 load_dotenv()  # load all the variables from the env file
+
+
+class BotClient(discord.Client):
+    async def on_ready(self):
+        print(f"{self.user} is ready and online!")
+
+    async def on_message(self, message):
+        if message.content == "hi":
+            await message.channel.send(
+                f"hello {message.author.mention}", reference=message
+            )
+
+
+# bot = discord.Bot(intents=intents)
 intents = discord.Intents.default()
 intents.message_content = True
-bot = discord.Bot(intents=intents)
-
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user} is ready and online!")
-
-
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx):
-    await ctx.respond("Hey!")
-
-
-@bot.message_command(
-    name="Get Message ID"
-)  # creates a global message command. use guild_ids=[] to create guild-specific commands.
-async def get_message_id(
-    ctx, message: discord.Message
-):  # message commands return the message
-    await ctx.respond(f"Message ID: `{message.id}`")
-
-
-@bot.event
-async def on_message(message):
-    if message.content == "hi":
-        await message.channel.send("hello", reference=message)
-
-
+bot = BotClient(intents=intents)
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))  # run the bot with the token
+
+
+# @bot.event
+# async def on_ready():
+#     print(f"{bot.user} is ready and online!")
+
+
+# @bot.slash_command(name="hello", description="Say hello to the bot")
+# async def hello(ctx):
+#     await ctx.respond("Hey!")
+
+
+# @bot.event
+# async def on_message(message):
+#     if message.content == "hi":
+#         await message.channel.send("hello", reference=message)
