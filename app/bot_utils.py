88d129e4b203f622d83dce83f97168b04a212344
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-06-20 20:37:16 -0600

    first functioning example of the cog using ABC meta class

--- app/bot_utils.py
+++ app/bot_utils.py
@@ -1,11 +1,79 @@
 import discord
 from discord.ext import commands
+import abc
 from dotenv import load_dotenv
 
 load_dotenv()  # load all the variables from the env file
 
 
 ## __COGS__ ##
+
+
+class CustomMeta(abc.ABCMeta, type(commands.Cog)):
+    pass
+
+
+class MessageInteraction(commands.Cog, metaclass=CustomMeta):
+    """
+    Abstract class for message interaction cogs.
+
+    "Cog trigger" in this context means the content of the message that will trigger the Cog to run.
+    Each MessageInteraction Cog will listen for specific messages contents and respond to it accordingly.
+    Cogs will also isolate specific actions the user could do like, insert things, ask for help, etc.
+
+    Every cog has its own state to keep track of the last message sent by the bot
+    this way user can interact with these messages and get specific actions/responses
+
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.last_message_id = None
+
+    def is_bot_own_message(self, message):
+        return message.author.id == self.bot.user.id
+
+    @abc.abstractmethod
+    async def starter_message(self, message):
+        """First message sent when message content matches Cog trigger."""
+        pass
+
+    async def message_reply(self, message):
+        """Message when user replies to bot starter message"""
+        pass
+
+    @abc.abstractmethod
+    @commands.Cog.listener("on_message")
+    async def message_router(self, message):
+        """Routes message to appropriate function based on content."""
+        pass
+
+    def setup(bot):
+        bot.add_cog(MessageInteraction(bot))
+
+
+class TestCog(MessageInteraction):
+    # def __init__(self, bot):
+    #     super().__init__(bot)
+
+    async def starter_message(self, message):
+        starter_message = await message.channel.send(
+            content="Hi there! I'm a test cog!", reference=message
+        )
+        self.last_message_id = starter_message.id
+
+    async def message_reply(self, message):
+        await message.channel.send("You replied to my message!")
+
+    @commands.Cog.listener("on_message")
+    async def message_router(self, message):
+        if message.content.lower() == "test":
+            await self.starter_message(message)
+        elif message.reference and self.last_message_id == message.reference.message_id:
+            await self.message_reply(message)
+
+    def setup(bot):
+        bot.add_cog(TestCog(bot))
 
 
 class StarterMessage(commands.Cog):
@@ -52,7 +120,7 @@ class RandomMessage(commands.Cog):
         bot.add_cog(RandomMessage(bot))
 
 
-cogs = [StarterMessage, RandomMessage]
+cogs = [StarterMessage, RandomMessage, TestCog]
 
 
 ## __VIEWS__ ##
