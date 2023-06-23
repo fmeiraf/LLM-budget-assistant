7cd4b872526541e1fa23834ab31b70f27193a01a
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-06-22 21:46:02 -0600

    converted code to use ORM with SQLAchemy, also transfered the test for the database.py file

--- app/database.py
+++ app/database.py
@@ -1,70 +1,227 @@
-import psycopg2
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    Float,
+    MetaData,
+    Date,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import DeclarativeBase
 
-# create database class to handle all database interactions
+from datetime import datetime
+from random import randint
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Create the database engine
+db_config = {
+    "host": "localhost",  # running on docker locally
+    "port": "5432",
+    "database": "llm_finance",
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+}
+
+
+# Base = declarative_base()
+metadata_obj = MetaData(schema="llm_finances")
+
+
+class Base(DeclarativeBase):
+    metadata = metadata_obj
+
+
+class User(Base):
+    __tablename__ = "users"
+    # __table_args__ = {"schema": "llm_finances"}
+
+    user_id = Column(Integer, primary_key=True)
+    email = Column(String)
+    password = Column(String)
+    accounts = relationship("Account", backref="users")
+    categories = relationship("Category", backref="users")
+    transactions = relationship("Transaction", backref="users")
+
+
+class Account(Base):
+    __tablename__ = "accounts"
+    # __table_args__ = {"schema": "llm_finances"}
+
+    account_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    account_last_4_digits = Column(String(4))
+    account_name = Column(String)
+    user = relationship("User", backref="user_accounts")
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    # __table_args__ = {"schema": "llm_finances"}
+
+    category_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    category_name = Column(String)
+    user = relationship("User", backref="user_categories")
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+    # __table_args__ = {"schema": "llm_finances"}
+
+    transaction_id = Column(Integer, primary_key=True)
+    transaction_date = Column(Date)
+    transaction_description = Column(String)
+    credit = Column(Float)
+    debit = Column(Float)
+    account_id = Column(Integer, ForeignKey("accounts.account_id"))
+    category_id = Column(Integer, ForeignKey("categories.category_id"))
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    user = relationship("User", backref="user_transactions")
 
 
 class Database:
     def __init__(self, host: str, port: str, database: str, user: str, password: str):
-        self.connection = psycopg2.connect(
-            host=host,
-            port=port,
-            database=database,
-            user=user,
-            password=password,
+        self.engine = create_engine(
+            f"postgresql://{user}:{password}@{host}:{port}/{database}",
+            echo=True,  # Set to False to disable SQL query logging
         )
+        self.Session = sessionmaker(bind=self.engine)
+
+    def get_all_users(self):
+        session = self.Session()
+        users = session.query(User).all()
+        session.close()
+        return users
 
     def create_user(self, email: str, password: str):
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO llm_finances.users (email, password) VALUES (%s, %s) RETURNING user_id;",
-                    (email, password),
-                )
-                user_id = cursor.fetchone()[0]
-                return user_id
+        session = self.Session()
+        user = User(email=email, password=password)
+        session.add(user)
+        session.commit()
+        user_id = user.user_id
+        session.close()
+        return user_id
 
-    def create_credit_card(self, user_id: int, card_last_4_digits: str, card_name: str):
-        # check card_last_4_digits has indeed 4 digits and that all digits are numbers
-        if len(card_last_4_digits) != 4:
-            raise ValueError("card_last_4_digits must be 4 digits long")
-        if not card_last_4_digits.isdigit():
-            raise ValueError("card_last_4_digits must be all numbers")
+    def create_account(
+        self, user_id: int, account_last_4_digits: str, account_name: str
+    ):
+        if len(account_last_4_digits) != 4 or not account_last_4_digits.isdigit():
+            raise ValueError(
+                "account_last_4_digits must be 4 digits long and contain only numbers"
+            )
 
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO llm_finances.credit_cards (user_id, card_last_4_digits, card_name) VALUES (%s, %s, %s) RETURNING card_id;",
-                    (user_id, card_last_4_digits, card_name),
-                )
-                card_id = cursor.fetchone()[0]
-                return card_id
+        session = self.Session()
+        account = Account(
+            user_id=user_id,
+            account_last_4_digits=account_last_4_digits,
+            account_name=account_name,
+        )
+        session.add(account)
+        session.commit()
+        account_id = account.account_id
+        session.close()
+        return account_id
 
     def create_category(self, user_id: int, category_name: str):
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO llm_finances.categories (user_id, category_name) VALUES (%s, %s) RETURNING category_id;",
-                    (user_id, category_name),
-                )
-                category_id = cursor.fetchone()[0]
-                return category_id
+        session = self.Session()
+        category = Category(user_id=user_id, category_name=category_name)
+        session.add(category)
+        session.commit()
+        category_id = category.category_id
+        session.close()
+        return category_id
 
     def create_transactions(self, user_id: int, transactions: list):
-        with self.connection:
-            with self.connection.cursor() as cursor:
-                for transaction in transactions:
-                    cursor.execute(
-                        "INSERT INTO llm_finances.transactions (transaction_date, transaction_description, credit, debit, card_id, category_id, user_id) VALUES (%s, %s, %s, %s, %s, %s, %s);",
-                        (
-                            transaction["transaction_date"],
-                            transaction["transaction_description"],
-                            transaction["credit"],
-                            transaction["debit"],
-                            transaction["card_id"],
-                            transaction["category_id"],
-                            user_id,
-                        ),
-                    )
+        session = self.Session()
+        for transaction in transactions:
+            transaction_obj = Transaction(
+                transaction_date=transaction["transaction_date"],
+                transaction_description=transaction["transaction_description"],
+                credit=transaction["credit"],
+                debit=transaction["debit"],
+                account_id=transaction["account_id"],
+                category_id=transaction["category_id"],
+                user_id=user_id,
+            )
+            session.add(transaction_obj)
+        session.commit()
+        session.close()
 
-    def close(self):
-        self.connection.close()
+
+## Test
+
+# Populate the database with some test data
+# Import the necessary libraries and classes
+
+
+# Create an instance of the Database class with your PostgreSQL credentials
+def populate_test_db():
+    db = Database(**db_config)
+
+    # Test data for creating a user
+    email = "test@example.com"
+    password = "testpassword"
+
+    # Test data for creating an account
+    card_last_4_digits = "1234"
+    account_name = "Test Card"
+
+    # Test data for creating a category
+    category_name = "Test Category"
+
+    # Test data for creating transactions
+    transactions = [
+        {
+            "transaction_date": datetime.now(),
+            "transaction_description": "Transaction 1",
+            "credit": 100,
+            "debit": 0,
+            "account_id": None,  # Replace with the appropriate card ID
+            "category_id": None,  # Replace with the appropriate category ID
+        },
+        {
+            "transaction_date": datetime.now(),
+            "transaction_description": "Transaction 2",
+            "credit": 0,
+            "debit": 50,
+            "account_id": None,  # Replace with the appropriate card ID
+            "category_id": None,  # Replace with the appropriate category ID
+        },
+    ]
+
+    # Query all users
+    # users = db.get_all_users()
+    # for user in users:
+    #     print(user.user_id, user.email, user.password)
+
+    # Create a user and retrieve the user ID
+    user_id = db.create_user(email, password)
+
+    # Create an account for the user and retrieve the card ID
+    account_id = db.create_account(user_id, card_last_4_digits, account_name)
+
+    # Create a category for the user and retrieve the category ID
+    category_id = db.create_category(user_id, category_name)
+
+    # Update the transactions with the card ID and category ID
+    for transaction in transactions:
+        transaction["account_id"] = account_id
+        transaction["category_id"] = category_id
+
+    # Create the transactions for the user
+    db.create_transactions(user_id, transactions)
+
+    # Close the database connection
+
+    print("Test records have been inserted successfully.")
+
+
+if __name__ == "__main__":
+    populate_test_db()
