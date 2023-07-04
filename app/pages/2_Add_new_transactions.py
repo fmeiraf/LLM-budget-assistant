dc070187f5c31e0321fd1c669ad1e45ec5d9aed4
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-07-03 21:23:56 -0600

    revamped the forms flow to not use empty containers anymore

--- app/pages/2_Add_new_transactions.py
+++ app/pages/2_Add_new_transactions.py
@@ -14,36 +14,35 @@ if "parsed_transactions" not in st.session_state:
 
 
 def main():
-    if st.session_state["logged_in"]:
-        login_status()
-    st.write("# Add new transactions 💸")
-    add_vertical_space(1)
-
-    st.markdown(
-        """Here is where you can add transactions to your accounts: 
-        """,
-        unsafe_allow_html=True,
-    )
-
-    if st.session_state["logged_in"]:
-        expander = st.expander("Instructions to add transactions:")
-        expander.write(
-            """- First, select the account you want to add transactions to.\
-        \n- Go to your account's portal (at your bank, credit card vendor, etc).\
-        \n- Copy and paste the transaction history into the text box below.\
-        \n- You can add multiple transactions at once.
-        \n- Make sure you have at list the transaction date, description, and amount.\
-        \n- Click the "Add Transactions" button.\
-        \n- After we proceess your transactions, you will be able see a summary of them.
-        \n- You can edit or delete transactions from the summary page.
-        """
-        )
-        add_vertical_space(2)
-        add_new_transactions__()
-
-    else:
+    if "logged_in" not in st.session_state or not st.session_state["logged_in"]:
         st.warning("Please log in to continue.")
         login()
+    else:
+        login_status()
+        st.write("# Add new transactions 💸")
+        add_vertical_space(1)
+
+        st.markdown(
+            """Here is where you can add transactions to your accounts: 
+            """,
+            unsafe_allow_html=True,
+        )
+
+        if st.session_state["logged_in"]:
+            expander = st.expander("Instructions to add transactions:")
+            expander.write(
+                """- First, select the account you want to add transactions to.\
+            \n- Go to your account's portal (at your bank, credit card vendor, etc).\
+            \n- Copy and paste the transaction history into the text box below.\
+            \n- You can add multiple transactions at once.
+            \n- Make sure you have at list the transaction date, description, and amount.\
+            \n- Click the "Add Transactions" button.\
+            \n- After we proceess your transactions, you will be able see a summary of them.
+            \n- You can edit or delete transactions from the summary page.
+            """
+            )
+            add_vertical_space(2)
+            add_new_transactions__()
 
 
 if __name__ == "__main__":
