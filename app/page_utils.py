30eb6b48e9dd1423bbe87dc59ca5680c60b3404b
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-07-26 20:15:15 -0600

    added stacked bar chart

--- app/page_utils.py
+++ app/page_utils.py
@@ -352,6 +352,7 @@ def category_stacked_bars(data=pd.DataFrame()):
             .groupby(["transaction_month", "category"])
             .agg({"amount": "sum"})
             .reset_index()
+            .sort_values("category")
         )
         # Stacked bar chart
         stacked_bar_chart = px.bar(
