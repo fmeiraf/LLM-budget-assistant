a27c07819b6e609870ed9ff2d52213e07df8afd6
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-08-29 22:01:36 -0600

    created new starting point for app so I can run database trigger right from the get go and just once

---
+++ app/start_app.py
@@ -0,0 +1,61 @@
+# run_streamlit.py
+import subprocess
+from database import Database, db_config
+from sqlalchemy import text
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+database = Database(**db_config)
+DEFAULT_ADD_CREDITS = 5
+DEFAULT_QUERY_CREDITS = 10
+
+
+def start_streamlit():
+    # run PostgreSQL triggers
+
+    ## trigger SQL
+
+    trigger_function_sql = text(
+        f"""
+    CREATE OR REPLACE FUNCTION add_credit_on_user_insertion()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        INSERT INTO llm_finances.credits(user_id, user_parsing_credit, user_query_credit)
+        VALUES (NEW.user_id, {DEFAULT_ADD_CREDITS}, {DEFAULT_QUERY_CREDITS});
+        RETURN NEW;
+    END;
+    $$ LANGUAGE plpgsql;
+    """
+    )
+
+    trigger_sql = text(
+        """
+    CREATE TRIGGER trigger_add_credit
+    AFTER INSERT ON llm_finances.users
+    FOR EACH ROW
+    EXECUTE FUNCTION add_credit_on_user_insertion();
+    """
+    )
+
+    session = database.Session()
+    session.execute(trigger_function_sql)
+    session.execute(trigger_sql)
+    session.commit()
+    session.close()
+
+    # The command to run streamlit
+
+    cmd = ["streamlit", "run", "app/Home.py"]
+    # Use subprocess to run the command
+    process = subprocess.Popen(cmd)
+
+    # You can wait for the process to complete with the following line,
+    # but this will block your script until the Streamlit server is terminated.
+    # process.wait()
+
+
+if __name__ == "__main__":
+    start_streamlit()
