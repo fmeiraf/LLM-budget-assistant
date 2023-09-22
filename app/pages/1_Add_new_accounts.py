49340dabefd12ca11e0c96de551dc656386ff19d
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-09-21 20:03:04 -0600

    transfered code for add_account to page file

--- app/pages/1_Add_new_accounts.py
+++ app/pages/1_Add_new_accounts.py
@@ -1,33 +1,48 @@
 import streamlit as st
 from streamlit_extras.add_vertical_space import add_vertical_space
 
-from page_utils import add_new_accounts, login_status, login
+from page_utils import add_new_accounts, login_status, login, database
 
 
 def main():
-    if st.session_state["logged_in"]:
-        login_status()
-
     st.write("# Add New accounts  💳")
     add_vertical_space(2)
 
-    st.markdown(
-        """Here is where you can add new accounts. Accounts represent the origins of your transactions.
-        """,
-        unsafe_allow_html=True,
-    )
-
-    st.markdown(
-        """ For example: here is where you would add your <b>credit cards</b>, <b>bank accounts</b>, and <b>cash accounts</b>.
-        """,
-        unsafe_allow_html=True,
-    )
-
-    if st.session_state["logged_in"]:
-        add_new_accounts()
-    else:
+    if "logged_in" not in st.session_state or not st.session_state["logged_in"]:
         st.warning("Please log in to continue.")
         login()
+    else:
+        login_status()
+
+        st.markdown(
+            """Here is where you can add new accounts. Accounts represent the origins of your transactions.
+            """,
+            unsafe_allow_html=True,
+        )
+
+        st.markdown(
+            """ For example: here is where you would add your <b>credit cards</b>, <b>bank accounts</b>, and <b>cash accounts</b>.
+            """,
+            unsafe_allow_html=True,
+        )
+
+        if st.session_state["logged_in"]:
+            account_name = st.text_input("Account Name")
+            account_last_digits = st.text_input("Last 4 digits (optional)", max_chars=4)
+
+            if st.button("Add Account"):
+                if account_name:
+                    if not account_last_digits:
+                        account_last_digits = "0102"
+
+                    database.create_account(
+                        user_id=st.session_state["user_id"],
+                        account_last_4_digits=account_last_digits,
+                        account_name=account_name,
+                    )
+                    st.success("Account added successfully.")
+                else:
+                    st.warning("Please enter an account name.")
 
 
 if __name__ == "__main__":
