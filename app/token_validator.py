5df38ab953a063c107f6e1b20b6154f010b348be
Luiz Fernando Meira Filho <fmeira.filho@gmail.com>
2023-09-28 19:59:53 -0600

    changed max_token_available logic and made it split into lesser chunker

--- app/token_validator.py
+++ app/token_validator.py
@@ -10,7 +10,7 @@ from rich import print as rprint
 dotenv.load_dotenv()
 
 MODEL = os.getenv("OPENAI_MODEL_NAME")
-MAX_TOKENS = 3000
+MAX_TOKENS = 3800
 
 
 class TokenValidator:
@@ -57,8 +57,8 @@ class TokenValidator:
         self.base_prompt_tokens = self.get_token_count(self.base_prompt)
 
         # getting max transactions tokens for future use
-        self.max_transactions_tokens = int(
-            (self.max_tokens_threshold - (2 * self.base_prompt_tokens)) / 3
+        self.max_transaction_input_tokens = int(
+            (self.max_tokens_threshold / 2) - self.base_prompt_tokens
         )
 
         self.initial_set_up_done = True
@@ -67,18 +67,27 @@ class TokenValidator:
         if not self.initial_set_up_done:
             self.set_up_initial_counts()
 
-        return (
-            (self.base_prompt_tokens + transaction_input_tokens) * 2
-        ) + transaction_input_tokens
+        return (self.base_prompt_tokens + transaction_input_tokens) * 2
 
     def split_tokens(self, transaction_input: str):
         if not self.initial_set_up_done:
             self.set_up_initial_counts()
         transaction_input_token_count = self.get_token_count(transaction_input)
-        # transaction_input_tokens = self.encode(transaction_input)
+
+        ## There area 2 base information that matters here
+        # The amount of tokens available based on prompt + transaction input size
+        # The amount that could be used for the transaction input
+
+        # in our case the prompt consists of guardrails support prompt + transaction input
+        # once transaction input is what will be parsed for final result we could expect that at max
+        # we would get 2x the transaction input tokens + prompt
+        max_available_tokens = int(
+            self.max_tokens_threshold
+            - ((transaction_input_token_count + self.base_prompt_tokens) * 2)
+        )
 
         # when the input is less than the max tokens threshold
-        if transaction_input_token_count <= self.max_transactions_tokens:
+        if max_available_tokens >= 0:
             self.result.append(
                 (
                     transaction_input,
@@ -96,7 +105,7 @@ class TokenValidator:
             token_per_transaction_cumsum = np.cumsum(token_per_transaction)
 
             max_token_index = np.argwhere(
-                token_per_transaction_cumsum > self.max_transactions_tokens
+                token_per_transaction_cumsum > self.max_transaction_input_tokens
             )[0][0]
 
             # slip the transactions in [left, right] format
@@ -107,7 +116,9 @@ class TokenValidator:
             self.result.append(
                 (
                     left_transaction_string,
-                    self.calculate_expected_final_tokens(self.max_transactions_tokens),
+                    self.calculate_expected_final_tokens(
+                        self.max_transaction_input_tokens
+                    ),
                 )
             )
             # split the remaining tokens
@@ -122,11 +133,25 @@ class TokenValidator:
 if __name__ == "__main__":
     from rich import print as rprint
 
-    with open("./app/test_data/long_transaction_td.txt", "r") as f:
-        test_input = f.read()
+    # with open("./app/test_data/test_data_no_header.txt", "r") as f:
+    #     test_input = f.read()
+
+    test_input = """Jul 22, 2023	AMZ*Sleepsheep Direct		$141.75	$4,838.90
+Jul 22, 2023	FIVE GUYS BURGERS & FR	$71.12		$4,980.65
+Jul 22, 2023	ABC*ANYTIME FITNESS	$28.34		$4,909.53
+Jul 21, 2023	RING MULTICAM MTH PLAN	$15.75		$4,881.19
+Jul 19, 2023	DOORDASH*LUNA MEXICAN	$130.82		$4,865.44
+Jul 15, 2023	AMZN Mktp CA*F307K57V3	$39.86		$4,315.77
+Jul 14, 2023	AMZN Mktp CA*WV57D4SK3	$33.59		$4,275.91
+Jul 13, 2023	adidas 2021 Edmonton	$90.32		$3,242.59
+Jul 12, 2023	NIKE CANADA - EDMONTON	$580.71		$3,152.27
+Jul 12, 2023	GAP OUTLET CA 8893	$172.73		$2,571.56
+Jul 10, 2023	APPLE STORE #R411	$10.00		$2,398.83"""
 
     guard = gd.Guard.from_rail_string(rail_str)
     tv = TokenValidator(model=MODEL, base_prompt=guard.base_prompt)
+
+    # rprint(tv.split_transactions(test_input))
 
     # rprint(tv.split_transactions(test_input))
 
